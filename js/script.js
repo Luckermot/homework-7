11ceb160e433f27adce39f11a54d4346e016e5f7
Ivan <errorka.01@mail.ru>
2023-02-01 15:22:49 +0300

    homework

--- js/script.js
+++ js/script.js
@@ -31,4 +31,42 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+
+  //time
+  const timeEnd = "2023-02-05";
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((t / 100 / 60) % 60),
+      seconds = Math.floor((t / 1000) % 60);
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = document.querySelector("#days"),
+      hours = document.querySelector("#hours"),
+      minutes = document.querySelector("#minutes"),
+      seconds = document.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      days.innerHTML = t.days;
+      hours.innerHTML = t.hours;
+      minutes.innerHTML = t.minutes;
+      seconds.innerHTML = t.seconds;
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock(".timer", timeEnd);
 });
